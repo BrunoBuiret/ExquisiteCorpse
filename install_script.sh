8cacfdb919e85336b1a731e111a61ea9b080146b
GUYL Bastien <bastien@guyl.me>
2016-12-12 16:47:37 +0100

    Add 2 hosts to Traefik

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,13 @@
+#! /bin/bash
+
 sudo yum update -y
 sudo yum install -y docker git
 sudo service docker start
-sudo usermod -a -G docker ec2-user #Disconnect
+# sudo usermod -a -G docker ec2-user #Disconnect
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose/
+
+git clone https://github.com/BrunoBuiret/ExquisiteCorpse
+
 cd exquisitecorpse docker-compose
