2c1361654a0ff0b29679dff06fc79f131e826723
Vil <vilghost@gmail.com>
2016-12-13 18:47:48 +0100

    Acme volume

--- install_ec.sh
+++ install_ec.sh
@@ -5,4 +5,6 @@ service docker start
 curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose/
 wget http://guyl.me/exquisitecorpse/docker-compose.yml
+mkdir /opt/acme/
+touch /opt/acme/acme.json
 docker-compose up -d
